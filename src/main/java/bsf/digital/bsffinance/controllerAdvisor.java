5387e438d2b34e0ddf8e695d4dfc6502380a4007
EvraamHany <evramhany2@gmail.com>
2022-10-08 01:55:06 +0200

    fix exception response

--- src/main/java/bsf/digital/bsffinance/controllerAdvisor.java
+++ src/main/java/bsf/digital/bsffinance/controllerAdvisor.java
@@ -1,7 +1,9 @@
-package bsf.digital.bsffinance.controller;
+package bsf.digital.bsffinance;
 
 import bsf.digital.bsffinance.exceptions.AccountNotExist;
 import bsf.digital.bsffinance.exceptions.NotValidTransaction;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,31 +11,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class controllerAdvisor extends ResponseEntityExceptionHandler {
 
 
     @ExceptionHandler(value= AccountNotExist.class)
     protected ResponseEntity<Object> accountNotExist(RuntimeException ex, WebRequest request) {
-
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("message", ex.getMessage());
-
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(value= NotValidTransaction.class)
     protected ResponseEntity<Object> notValidTransaction(RuntimeException ex, WebRequest request) {
-
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("message", ex.getMessage());
-
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
