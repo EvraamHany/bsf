11045af42eb474554c60886ab5cd24303925aabf
EvraamHany <evramhany2@gmail.com>
2022-10-08 00:18:45 +0200

    fix swagger issue

--- src/main/java/bsf/digital/bsffinance/Application.java
+++ src/main/java/bsf/digital/bsffinance/Application.java
@@ -2,13 +2,15 @@ package bsf.digital.bsffinance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
 	}
-	
 }
